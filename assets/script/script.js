0b745fa3505f4648c68a4e474678593eab18b5e1
Diane-Ring <diane-hawkins@hotmail.co.uk>
2025-11-18 16:46:11 +0000

    Add words and definitions to script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,24 @@
+{
+  text: "What does Happy mean",
+  options: ["Feeling tired and sleepy", "Being angry or upset", "Feeling good or joyful", "Wanting to cry"],
+},
 
+{
+  text: "What does Big mean",
+  options: ["Fast and speedy", "Bright and colorful", "Large in size", "Very small or tiny"],
+},
+
+{
+  text: "What does Jump mean",
+  options: ["To eat something quickly", "To push yourself off the ground", "To sleep in a bed", "To sit down quietly"],
+},
+
+{
+  text: "What does Red mean",
+  options: ["A shape with three sides", "A type of animal", "A sound made by a bell", "A bright color like an apple"],
+},
+
+{
+  text: "What does Cold mean",
+  options: ["Not warm; chilly", "Soft and furry", "Full of sunshine and heat", "Very loud and noisy"],
+},
